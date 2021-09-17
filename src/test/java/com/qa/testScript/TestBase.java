13e2fd0ed18aa7fd1470aa3c28855887af746f93
VAMSHHI000 <vamsikrishnareddy.kondapally@gmail.com>
2021-09-17 19:52:14 +0530

    Basic Project

---
+++ src/test/java/com/qa/testScript/TestBase.java
@@ -0,0 +1,130 @@
+package com.qa.testScript;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import com.qa.pages.RecruitmentPages;
+import com.qa.utility.ExcelUtility;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class TestBase {
+	WebDriver driver=null;
+	RecruitmentPages recruitmentPages;
+	
+	@Parameters({ "OrangeHrmURL","Browser"})
+	@BeforeClass
+	public void Setup(String OrangeHrmURL,String browserName) throws IOException {
+		if (browserName.equalsIgnoreCase("Chrome")) {
+			getProperties();
+			WebDriverManager.chromedriver().setup();
+			driver =new ChromeDriver();
+		
+		}		else if (browserName.equalsIgnoreCase("Edge")) {
+
+			WebDriverManager.edgedriver().setup();
+
+			driver =new EdgeDriver();
+			
+
+
+		}
+		recruitmentPages = new RecruitmentPages(driver);
+		driver.get(OrangeHrmURL);
+		
+		
+		Properties properties= new Properties();
+		String projectPath=System.getProperty("user.dir");
+		InputStream input= new FileInputStream(projectPath+"\\src\\test\\java\\com\\qa\\testData\\login.properties");
+		properties.load(input);
+		
+		recruitmentPages.userName().sendKeys(properties.getProperty("UserName"));
+		recruitmentPages.password().sendKeys(properties.getProperty("Password"));
+		recruitmentPages.login().click();
+		
+
+
+
+	}
+
+	@AfterClass
+	public void TearDown() throws InterruptedException {
+
+		driver.quit();
+	}
+
+	public void captureScreenshot(WebDriver driver,String tname) throws IOException {
+
+		TakesScreenshot ts = (TakesScreenshot)driver;
+
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File target= new File(System.getProperty("user.dir")+"/Screenshots/"+tname+".png");
+
+		FileUtils.copyFile(source, target);
+		System.out.println("Screenshot captured");
+
+
+
+	}	
+
+	public String[][] getExcelData(String sheet) throws IOException{
+		String path="D:\\Automation\\Testing\\orangeHrmDemo\\src\\test\\java\\com\\qa\\testData\\orangeHrm_inputData.xlsx"; 
+		ExcelUtility excelUtils =new ExcelUtility(path,sheet);
+
+		int rowCount=excelUtils.getRowCount();
+		int colCount=excelUtils.getColCount();
+
+		String data[][] = new String[rowCount-1][colCount];
+
+		for(int i=1;i<rowCount;i++) {
+			for(int j=0;j<colCount;j++) {
+				if(excelUtils.getCellType(i, j)) {
+					String cellData= excelUtils.getCellDataString(i, j);
+
+					data[i-1][j]=cellData;
+				}	else {
+					double cellData= excelUtils.getCellNumaricString(i, j);
+
+					int intVal=(int)cellData;
+					String s=Integer.toString(intVal);
+					data[i-1][j]=s;
+
+				}
+
+			}
+		}
+
+
+		return data;
+
+
+
+	}
+	
+	public void getProperties() throws IOException {
+		
+		Properties properties= new Properties();
+		String projectPath=System.getProperty("user.dir");
+		InputStream input= new FileInputStream(projectPath+"\\src\\test\\java\\com\\qa\\testData\\login.properties");
+		properties.load(input);
+		System.out.println(properties.getProperty("UserName")+"   "+properties.getProperty("Password"));
+		
+	}
+
+
+}
